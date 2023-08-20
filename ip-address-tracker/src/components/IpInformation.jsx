46a71da75a96c837a549196fe74611c00b672672
Reymart <vigoreymart11@gmail.com>
2023-08-19 19:17:10 -1000

    latest-commit

--- ip-address-tracker/src/components/IpInformation.jsx
+++ ip-address-tracker/src/components/IpInformation.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const IpInformation = ({ ipAddress, country, region, postalcode, timezone, isp }) => {
     return (
-        <div className="rounded-xl bg-White sm:w-10/12 sm:flex sm:flex-col sm:gap-4 sm:items-center sm:py-8 lg:flex-row lg: justify-between  lg:py-10 lg:-mt-4 lg2:mt-0 xl:w-8/12">
+        <div className="rounded-xl bg-White sm:w-10/12 sm:flex sm:flex-col sm:gap-4 sm:items-center sm:py-8 lg:flex-row lg: justify-between  lg:py-10 lg:-mt-4 lg2:mt-0 xl:w-8/12 xl:items-start">
             <div className="sm:flex sm:flex-col sm:items-center gap-2 lg:items-start lg:px-4 lg2:px-8">
                 <span className="text-DarkGray font-semibold sm:text-sm tracking-widest ">IP ADDRESS</span>
                 <h2 className="text-VeryDarkGray font-medium sm:text-xl xl:text-header">{ipAddress}</h2>
             </div>
 
             {country && region && (
-                <div className="sm:flex sm:flex-col sm:items-center gap-2 lg:items-start  lg:border-l-2 border-DarkGray lg:px-4 lg2:px-8">
+                <div className="sm:flex sm:flex-col sm:items-center gap-2 lg:items-start  lg:border-l-2  lg:border-r-2 border-DarkGray lg:px-4 lg2:px-8">
                     <span className="text-DarkGray font-semibold sm:text-sm tracking-widest" >LOCATION</span>
                     <h2 className="text-VeryDarkGray font-medium sm:text-xl xl:text-header">{region},{country} </h2>
                 </div>
@@ -17,9 +17,9 @@ const IpInformation = ({ ipAddress, country, region, postalcode, timezone, isp }
 
 
             {timezone && (
-                <div className="sm:flex sm:flex-col sm:items-center gap-2 lg:items-start  lg:border-l-2 border-DarkGray lg:px-4 xl:py-4 lg2:px-8  xl:h-full">
+                <div className="sm:flex sm:flex-col sm:items-center gap-2 lg:items-start   border-DarkGray lg:px-4  lg2:px-6  xl:h-full">
                     <span className="text-DarkGray font-semibold sm:text-sm tracking-widest">TIMEZONE</span>
-                    <h2 className="text-VeryDarkGray font-medium sm:text-xl xl:text-header">{timezone}</h2>
+                    <h2 className="text-VeryDarkGray font-medium sm:text-xl xl:text-header">UTC{timezone}</h2>
                 </div>
             )}
 
